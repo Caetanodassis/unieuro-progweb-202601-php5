3b3abb5845030f5e6f88a2052fcf630c16cb34ad
VINICIUS CAETANO DE ASSIS <150752924+Caetanodassis@users.noreply.github.com>
2026-04-01 09:56:50 -0300

    Delete progweb04042025 directory

--- progweb04042025/menu.php
+++
@@ -1,9 +0,0 @@
-<div class="navbar">
-        <div class="logo">Minha Empresa</div>
-        <nav>
-            <a href="index.php">Home</a>
-            <a href="usuarios.php">Usuários</a>
-            <a href="produtos.php">Produtos</a>
-            <a href="pedidos.php">Pedidos</a>
-        </nav>
-    </div>
